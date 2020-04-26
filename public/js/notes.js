89f1df0afd5a160c1967bc2d62d4eb11d835b51e
kalyan chakravarthi <kalyanveenam@gmail.com>
2020-04-26 09:26:37 +0530

    Added code for delete functionality

--- public/js/notes.js
+++ public/js/notes.js
@@ -4,6 +4,7 @@ var btn_submit = document.getElementById("btn-addNote");
 const btn_getNotes = document.getElementById("btn-getNote");
 const div_notes = document.getElementById("div-msg");
 const btn_delete = document.getElementById("btn-delNote");
+const btn_refresh=document.getElementById('btn-refNote');
 Handlebars.registerHelper("data", function (property) {
   return property;
 });
@@ -60,13 +61,21 @@ btn_getNotes.addEventListener("click", function (e) {
       console.log(error);
     });
 });
-
+function getIdOnClick(id) 
+{
+  console.log(id)
+  document.getElementById('div-dummy').innerHTML=id; 
+  
+}
 document.getElementById("btn-delNote").addEventListener("click", function (e) {
+  const get_id=document.getElementById('div-dummy').innerHTML;
+  console.log('id:'+get_id)
   const data = {
-    _id: "5e9da08ea8063d29b360a8bd",
+    _id: get_id,
   };
-
-  fetch("/notes", {
+  const url ='/notes/delete/'+get_id
+  console.log(url)
+  fetch(url, {
     method: "delete",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
@@ -74,8 +83,40 @@ document.getElementById("btn-delNote").addEventListener("click", function (e) {
     body: JSON.stringify(data),
   }).then((req, res) => {
     console.log("Note is deleted");
+  
   });
+
 });
-document.getElementById("div-dispNote").addEventListener("click", function () {
-  console.log("clicked");
+btn_refresh.addEventListener("click", function (e) {
+  fetch("/notes", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  })
+    .then(function (data) {
+      data.json().then((data) => {
+        var notes_data = {};
+        notes_data.notes = data;
+        var data = notes_data;
+        var note_data = document.getElementById("temp-getData").innerHTML;
+        var template = Handlebars.compile(note_data);
+        var note_dt = template(data);
+        console.log(data);
+        $(document).ready(function () {
+          $(".view-details").on("click", function (e) {
+            e.preventDefault();
+            console.log("clicked");
+          });
+        });
+        document.getElementById("div-dispNote").innerHTML = note_dt;
+        const data_id = document.getElementById("div-dispNote").innerHTML;
+        console.log(data_id);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
+
+
